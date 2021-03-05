0d8239a4d6c75f79d2ebd944c756c453ea6bb700
MayurBhowad <mayurbhowad1995@gmail.com>
2021-03-05 18:59:35 +0530

    render_templating

--- Flask_Blog/FlaskBlog.py
+++ Flask_Blog/FlaskBlog.py
@@ -1,15 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog post 1',
+        'content': 'First content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Blog post 2',
+        'content': 'Second content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello,, World!</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about_page():
-    return '<h1>About Page!</h1>'
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact_page():
+    return '<h1>contact Page!</h1>'
 
 
 if __name__ == '__main__':
